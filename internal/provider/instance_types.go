8631aea136f09ea9d2bd10f076f02ef6aa5f8bb0
Simon Kirsten <skirsten@genesiscloud.com>
2023-08-18 14:22:52 +0200

    Documentation Improvements

--- internal/provider/instance_types.go
+++ internal/provider/instance_types.go
@@ -12,7 +12,7 @@ import (
 type InstanceMetadataModel struct {
 	// StartupScript A plain text bash script or "cloud-config" file that will be executed after the first instance boot.
 	// It is limited to 64 KiB in size. You can use it to configure your instance, e.g. installing the **NVIDIA GPU driver**.
-	// Learn more about [startup scripts and installing the GPU driver](https://support.com/support/solutions/articles/47001122478).
+	// Learn more about [startup scripts and installing the GPU driver](https://support.genesiscloud.com/support/solutions/articles/47001122478).
 	StartupScript types.String `tfsdk:"startup_script"`
 }
 
