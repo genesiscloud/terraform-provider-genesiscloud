2d8733fa1df8903c465b5a8695213bc99a79e35c
Philip Krück <hi@philipkrueck.com>
2024-02-12 15:40:44 +0700

    update local-deploy

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 version="0.0.0-local"
-platform="darwin_arm64"
+platform="darwin_arm64" # check for your OS using `uname -sm`
 
 mkdir -p ~/.terraform.d/plugins/registry.terraform.io/genesiscloud/genesiscloud/$version/$platform
 cp terraform-provider-genesiscloud ~/.terraform.d/plugins/registry.terraform.io/genesiscloud/genesiscloud/$version/$platform/terraform-provider-genesiscloud
