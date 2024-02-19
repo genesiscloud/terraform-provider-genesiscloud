e34a8f24b23c02c97294a040d84381bc31bf1f4a
Simon Kirsten <1972314+skirsten@users.noreply.github.com>
2024-02-19 15:55:52 +0100

    Remove snapshot size parsing (#52)

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 version="0.0.0-local"
-platform="darwin_arm64" # use `uname -sm` to get the current platform
+platform="$(go env GOOS)_$(go env GOARCH)"
 
 mkdir -p ~/.terraform.d/plugins/registry.terraform.io/genesiscloud/genesiscloud/$version/$platform
 cp terraform-provider-genesiscloud ~/.terraform.d/plugins/registry.terraform.io/genesiscloud/genesiscloud/$version/$platform/terraform-provider-genesiscloud
